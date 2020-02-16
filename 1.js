6bfa6816c46139df210940b2d925af0dd6936bb4
TanyaKunts <kuntsevichtatiana@gmail.com>
2020-02-16 22:31:50 +0300

    dz

---
+++ 1.js
@@ -0,0 +1,12 @@
+const N1=prompt("Введите первое число: ");
+const N2=prompt("Введите второе число: ");
+const N3=prompt("Введите третье число: ");
+const N4=prompt("Введите четвертое число: ");
+const A = Number(N1);
+const B = Number(N2);
+const C = Number(N3);
+const D = Number(N4);
+X=(A>B) ? A : B;
+Y=(C>D) ? C : D;
+alert((X>Y) ? X : Y);
+alert(4/((1/N1)+(1/N2)+(1/N3)+(1/N4)))
