52e3d8283f82a35aa9e9623ecee43458c0d4c5d9
TanyaKunts <kuntsevichtatiana@gmail.com>
2020-02-17 14:18:29 +0300

    scripts

--- 4.js
+++
@@ -1,6 +0,0 @@
-const B=prompt("Введите первое число: ");
-const A = Number(B);
-alert((A*10**(-3)));
-alert((A*10**(-6)));
-alert((A*10**(-9)));
-
--- 5.js
+++
@@ -1,8 +0,0 @@
-const N1=prompt("Введите первое число: ");
-const N2=prompt("Введите второе число: ");
-const N3=prompt("Введите третье число: ");
-const A = Number(N1);
-const B = Number(N2);
-const C = Number(N3);
-const p=((A+B+C)/2);
-alert((p*((p-A)*(p-B)*(p-C)))**1/2)
